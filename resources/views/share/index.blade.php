e2f7506220a69e4fa6fe82d2652c6a8109f837da
sabrina <wansabrina.ws@gmail.com>
2023-11-21 22:35:03 +0700

    add: Copy Key
    Co-authored-by: Rayssa Ravelia <rayssa.sibarani@gmail.com>
    
    Co-authored-by: Katarina Inezita <inezitakatarina16@gmail.com>

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -4,19 +4,20 @@
         <hr>
         <div class="container">
             @if(isset($key))
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <strong>Key anda!</strong>
-                    <p class="text-break my-1 fs-6">{{ $key }}</p>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Key anda!</strong>
+                <p id="keyText" class="text-break my-1 fs-6">{{ $key }}</p>
+                <button type="button" class="btn btn-secondary btn-sm mt-2" onclick="copyToClipboard()">Copy Key</button>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
             @endif
             <form action="{{ route('share.show') }}" method="get">
                 @csrf
                 @if ($errors->any())
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Error!</strong> {{ $errors->all()[0] }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> {{ $errors->all()[0] }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
                 @endif
                 <div class="form-outline mb-4">
                     <label class="form-label" for="nama">ID User Tujuan Enkripsi</label>
@@ -28,4 +29,29 @@
             </form>
         </div>
     </div>
+
+    <script>
+        function copyToClipboard() {
+            // Membuat text area sementara
+            var textArea = document.createElement("textarea");
+            // Mengambil teks dari elemen yang menampilkan key
+            textArea.value = document.getElementById("keyText").innerText;
+            document.body.appendChild(textArea);
+            textArea.select(); 
+            textArea.setSelectionRange(0, 99999); // Untuk mobile view
+
+            navigator.clipboard.writeText(textArea.value).then(function() {
+                console.log('Key successfully copied to clipboard');
+                var alertBox = document.querySelector('.alert strong');
+                if (alertBox) {
+                    alertBox.textContent = 'Key berhasil dicopy!';
+                }
+            }).catch(function(error) {
+                console.error('Error copying key to clipboard', error);
+            });
+
+            document.body.removeChild(textArea);
+        }
+    </script>
+
 </x-app-layout>
