1541cd3c0dd51bff3b6fbc36214e7f9b016fb7ca
ray <89933907+rayrednet@users.noreply.github.com>
2023-11-21 23:00:34 +0700

    add: Share Key via Whatsapp & Email" -m "Co-authored-by: Wan Sabrina Mayzura <wansabrina.ws@gmail.com>" -m "Co-authored-by: Katarina Inezita <inezitakatarina16@gmail.com>

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -8,6 +8,24 @@
                 <strong>Key anda!</strong>
                 <p id="keyText" class="text-break my-1 fs-6">{{ $key }}</p>
                 <button type="button" class="btn btn-secondary btn-sm mt-2" onclick="copyToClipboard()">Copy Key</button>
+                <!-- Modal Share Key -->
+                <div class="modal fade" id="shareKeyModal" tabindex="-1" aria-labelledby="shareKeyModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="shareKeyModalLabel">Share Key</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <button onclick="shareViaWhatsApp()" class="btn btn-success btn-sm mt-2">WhatsApp</button>
+                                <button onclick="shareViaEmail()" class="btn btn-primary btn-sm mt-2">Email</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-info btn-sm mt-2" data-bs-toggle="modal" data-bs-target="#shareKeyModal">
+                    Share Key
+                </button>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
             @endif
@@ -37,7 +55,7 @@
             // Mengambil teks dari elemen yang menampilkan key
             textArea.value = document.getElementById("keyText").innerText;
             document.body.appendChild(textArea);
-            textArea.select(); 
+            textArea.select();
             textArea.setSelectionRange(0, 99999); // Untuk mobile view
 
             navigator.clipboard.writeText(textArea.value).then(function() {
@@ -51,6 +69,18 @@
             });
 
             document.body.removeChild(textArea);
+        }
+
+        function shareViaWhatsApp() {
+            var key = document.getElementById("keyText").innerText;
+            var whatsappUrl = "https://wa.me/?text=" + encodeURIComponent(key);
+            window.open(whatsappUrl, '_blank');
+        }
+
+        function shareViaEmail() {
+            var key = document.getElementById("keyText").innerText;
+            var mailtoUrl = "mailto:?subject=Sharing Key&body=" + encodeURIComponent(key);
+            window.location.href = mailtoUrl;
         }
     </script>
 
